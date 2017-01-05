42238d543e54e20ffce5b366f450ab04c01aa1f8
李万冲 <liwanchong@navinfo.com>
2017-01-05 09:26:05 +0800

    导航栏

---
+++ js/constant/apisConstant.js
@@ -0,0 +1,265 @@
+/**
+ * Created by liwanchong on 2017/1/4.
+ */
+productServiceApp.constant('JumpConstant', {
+    Tile: [
+        {
+            name: '瓦片信息',
+            id: 'apiName'
+        },
+        {
+            name: 'API介绍',
+            id: 'apiExplain'
+        },
+        {
+            name: '适用场景',
+            id: 'useCase'
+        },
+        {
+            name: '使用说明',
+            id: 'instructions'
+        },
+        {
+            name: '获取瓦片ID',
+            id: 'tileNumber'
+        },
+        {
+            name: '批量获取瓦片ID',
+            id: 'tilesNumber'
+        }
+    ],
+    Mesh: [
+        {
+            name: '获取图幅信息',
+            id: 'apiName'
+        },
+        {
+            name: 'API介绍',
+            id: 'apiExplain'
+        },
+        {
+            name: '适用场景',
+            id: 'useCase'
+        },
+        {
+            name: '使用说明',
+            id: 'instructions'
+        },
+        {
+            name: '获取图幅ID',
+            id: 'getSingleMesh'
+        },
+        {
+            name: '批量获取图幅ID',
+            id: 'getMulitiMeshes'
+        },
+    ],
+    Pois: [
+        {
+            name: 'POI查询',
+            id: 'apiName'
+        },
+        {
+            name: 'API介绍',
+            id: 'apiExplain'
+        },
+        {
+            name: '适用场景',
+            id: 'useCase'
+        },
+        {
+            name: '使用说明',
+            id: 'instructions'
+        },
+    ],
+    Region: [
+        {
+            name: '行政区划查询',
+            id: 'apiName'
+        },
+        {
+            name: 'API介绍',
+            id: 'apiExplain'
+        },
+        {
+            name: '适用场景',
+            id: 'useCase'
+        },
+        {
+            name: '使用说明',
+            id: 'instructions'
+        },
+        {
+            name: '行政区划查询',
+            id: 'queryRegion'
+        }
+    ],
+    RoadName: [
+        {
+            name: '道路名查询',
+            id: 'apiName'
+        },
+        {
+            name: 'API介绍',
+            id: 'apiExplain'
+        },
+        {
+            name: '适用场景',
+            id: 'useCase'
+        },
+        {
+            name: '使用说明',
+            id: 'instructions'
+        },
+        {
+            name: '单个瓦片道路名查询',
+            id: 'singleTileRoadName'
+        }, {
+            name: '多个瓦片道路名查询',
+            id: 'mulitiTilesRoadName'
+        },
+    ],
+    Link: [
+        {
+            name: 'link信息查询',
+            id: 'apiName'
+        },
+        {
+            name: 'API介绍',
+            id: 'apiExplain'
+        },
+        {
+            name: '适用场景',
+            id: 'useCase'
+        },
+        {
+            name: '使用说明',
+            id: 'instructions'
+        },
+        {
+            name: ' 单个瓦片link信息查询',
+            id: 'singleTile'
+        },
+        {
+            name: '多个瓦片link信息查询',
+            id: 'mulitiTiles'
+        },
+        {
+            name: 'layer内link信息查询',
+            id: 'linkInLayer'
+        },
+        {
+            name: '自定义link信息查询',
+            id: 'definedLink'
+        }
+    ],
+    Layer: [
+        {
+            name: 'layer信息查询',
+            id: 'apiName'
+        },
+        {
+            name: 'API介绍',
+            id: 'apiExplain'
+        },
+        {
+            name: '适用场景',
+            id: 'useCase'
+        },
+        {
+            name: '使用说明',
+            id: 'instructions'
+        },
+        {
+            name: 'layer信息查询',
+            id: 'getLayerInfo'
+        }
+    ],
+    LayerStructure: [
+        {
+            name: '获取layer结构',
+            id: 'apiName'
+        },
+        {
+            name: 'API介绍',
+            id: 'apiExplain'
+        },
+        {
+            name: '适用场景',
+            id: 'useCase'
+        },
+        {
+            name: '使用说明',
+            id: 'instructions'
+        },
+        {
+            name: '获取layer结构',
+            id: 'getLayerStructure'
+        }
+    ],
+    Ak: [
+        {
+            name: '高程值',
+            id: 'apiName'
+        },
+        {
+            name: 'API介绍',
+            id: 'apiExplain'
+        },
+        {
+            name: '适用场景',
+            id: 'useCase'
+        },
+        {
+            name: '使用说明',
+            id: 'instructions'
+        },
+        {
+            name: '获取高程值',
+            id: 'akContainer'
+        }
+    ],
+    Tollcost: [
+        {
+            name: '路径收费',
+            id: 'apiName'
+        },
+        {
+            name: 'API介绍',
+            id: 'apiExplain'
+        },
+        {
+            name: '适用场景',
+            id: 'useCase'
+        },
+        {
+            name: '使用说明',
+            id: 'instructions'
+        },
+        {
+            name: '计算收费金额',
+            id: 'tollCostContainer'
+        },
+    ],
+    summary: [
+        {
+            name: '概述',
+            id: 'summary'
+        },
+        {
+            name: '简介',
+            id: 'summaryExplain'
+        }
+    ],
+    code: [
+        {
+            name: '状态码说明',
+            id: 'code',
+        },
+        {
+            name: '状态码介绍',
+            id: 'codeExplain',
+        }
+    ]
+
+});
--- js/productService.js
+++ js/productService.js
@@ -5,7 +5,7 @@ $(function () {
     $('#apiPage').load('../pages/api/getTileInfomation.html');
 });
 var productServiceApp = angular.module('productServiceApp', []);
-productServiceApp.controller('selectedController',function ($scope) {
+productServiceApp.controller('selectedController',function ($scope,JumpConstant) {
     $scope.services = [
         {
             name: '概述',
@@ -79,268 +79,6 @@ productServiceApp.controller('selectedController',function ($scope) {
             flag: false
         }
     ];
-    $scope.direct = {
-        Tile: [
-            {
-                name: '瓦片信息',
-                id: 'apiName'
-            },
-            {
-                name: 'API介绍',
-                id: 'apiExplain'
-            },
-            {
-                name: '适用场景',
-                id: 'useCase'
-            },
-            {
-                name: '使用说明',
-                id: 'instructions'
-            },
-            {
-                name: '获取瓦片ID',
-                id: 'tileNumber'
-            },
-            {
-                name: '批量获取瓦片ID',
-                id: 'tilesNumber'
-            }
-        ],
-        Mesh:[
-            {
-                name: '获取图幅信息',
-                id: 'apiName'
-            },
-            {
-                name: 'API介绍',
-                id: 'apiExplain'
-            },
-            {
-                name: '适用场景',
-                id: 'useCase'
-            },
-            {
-                name: '使用说明',
-                id: 'instructions'
-            },
-            {
-                name: '获取图幅ID',
-                id: 'getSingleMesh'
-            },
-            {
-                name: '批量获取图幅ID',
-                id: 'getMulitiMeshes'
-            },
-        ],
-        Pois:[
-            {
-                name: 'POI查询',
-                id: 'apiName'
-            },
-            {
-                name: 'API介绍',
-                id: 'apiExplain'
-            },
-            {
-                name: '适用场景',
-                id: 'useCase'
-            },
-            {
-                name: '使用说明',
-                id: 'instructions'
-            },
-        ],
-        Region:[
-            {
-                name: '行政区划查询',
-                id: 'apiName'
-            },
-            {
-                name: 'API介绍',
-                id: 'apiExplain'
-            },
-            {
-                name: '适用场景',
-                id: 'useCase'
-            },
-            {
-                name: '使用说明',
-                id: 'instructions'
-            },
-            {
-                name: '行政区划查询',
-                id: 'queryRegion'
-            }
-        ],
-        RoadName:[
-            {
-                name: '道路名查询',
-                id: 'apiName'
-            },
-            {
-                name: 'API介绍',
-                id: 'apiExplain'
-            },
-            {
-                name: '适用场景',
-                id: 'useCase'
-            },
-            {
-                name: '使用说明',
-                id: 'instructions'
-            },
-            {
-                name: '单个瓦片道路名查询',
-                id: 'singleTileRoadName'
-            },{
-                name: '多个瓦片道路名查询',
-                id: 'mulitiTilesRoadName'
-            },
-        ],
-        Link:[
-            {
-                name: 'link信息查询',
-                id: 'apiName'
-            },
-            {
-                name: 'API介绍',
-                id: 'apiExplain'
-            },
-            {
-                name: '适用场景',
-                id: 'useCase'
-            },
-            {
-                name: '使用说明',
-                id: 'instructions'
-            },
-            {
-                name: ' 单个瓦片link信息查询',
-                id: 'singleTile'
-            },
-            {
-                name: '多个瓦片link信息查询',
-                id: 'mulitiTiles'
-            },
-            {
-                name: 'layer内link信息查询',
-                id: 'linkInLayer'
-            },
-            {
-                name: '自定义link信息查询',
-                id: 'definedLink'
-            }
-        ],
-        Layer:[
-            {
-                name: 'layer信息查询',
-                id: 'apiName'
-            },
-            {
-                name: 'API介绍',
-                id: 'apiExplain'
-            },
-            {
-                name: '适用场景',
-                id: 'useCase'
-            },
-            {
-                name: '使用说明',
-                id: 'instructions'
-            },
-            {
-                name: 'layer信息查询',
-                id: 'getLayerInfo'
-            }
-        ],
-        LayerStructure:[
-            {
-                name: '获取layer结构',
-                id: 'apiName'
-            },
-            {
-                name: 'API介绍',
-                id: 'apiExplain'
-            },
-            {
-                name: '适用场景',
-                id: 'useCase'
-            },
-            {
-                name: '使用说明',
-                id: 'instructions'
-            },
-            {
-                name: '获取layer结构',
-                id: 'getLayerStructure'
-            }
-        ],
-        Ak:[
-            {
-                name: '高程值',
-                id: 'apiName'
-            },
-            {
-                name: 'API介绍',
-                id: 'apiExplain'
-            },
-            {
-                name: '适用场景',
-                id: 'useCase'
-            },
-            {
-                name: '使用说明',
-                id: 'instructions'
-            },
-            {
-                name: '获取高程值',
-                id: 'akContainer'
-            }
-        ],
-        Tollcost:[
-            {
-                name: '路径收费',
-                id: 'apiName'
-            },
-            {
-                name: 'API介绍',
-                id: 'apiExplain'
-            },
-            {
-                name: '适用场景',
-                id: 'useCase'
-            },
-            {
-                name: '使用说明',
-                id: 'instructions'
-            },
-            {
-                name: '计算收费金额',
-                id: 'tollCostContainer'
-            },
-        ],
-        summary: [
-            {
-                name: '概述',
-                id: 'summary'
-            },
-            {
-                name: '简介',
-                id: 'summaryExplain'
-            }
-        ],
-        code: [
-            {
-                name: '状态码说明',
-                id: 'code',
-            },
-            {
-                name: '状态码介绍',
-                id: 'codeExplain',
-            }
-        ]
-
-    };
     $scope.jumps = [
         {
             name: '瓦片信息',
@@ -374,7 +112,7 @@ productServiceApp.controller('selectedController',function ($scope) {
     };
     $scope.changeDataApiPage = function (parentName,type, name) {
         $scope.jumpFlag = '';
-        $scope.jumps = $scope.direct[type];
+        $scope.jumps = JumpConstant[type];
         $scope.jumpHeight.height = 30 * $scope.jumps.length + 'px';
         $scope.dataServiceFlag = type;
         $('#apiType').text(parentName);
@@ -384,7 +122,7 @@ productServiceApp.controller('selectedController',function ($scope) {
     };
     $scope.showArrowDirect = function (type, num) {
         $scope.jumpFlag = '';
-        $scope.jumps = $scope.direct[type];
+        $scope.jumps = JumpConstant[type];
         $scope.jumpHeight.height = 30 * $scope.jumps.length + 'px';
         if (type === 'summary') {
             $('#apiPage').empty();
