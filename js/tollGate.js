b5b65879d6d5031c6b36b2e6cbac3f995ded0390
李万冲 <liwanchong@navinfo.com>
2017-03-10 15:57:40 +0800

    links

--- js/tollGate.js
+++ js/tollGate.js
@@ -6,18 +6,14 @@ tollGate.controller("tollGateController", ['$scope', 'dsEdit', '$location', func
     $scope.param = {
         name: '福建'
     };
+    $scope.startTollGate = '';
+    $scope.endTollGate = '';
+    $scope.tollGateArr = [];
     $scope.popuArr = [];
-    $scope.clearLines = function () {
-        var geojson = {
-            "type": "FeatureCollection",
-            "features": []
-        };
-        map.getSource('route').setData(geojson);
-        for (var i = 0, len = $scope.popuArr.length; i < len; i++) {
-            $scope.popuArr[i].remove();
-        }
-        $scope.popuArr.length = 0;
-    };
+    $scope.startFlag = false;
+    $scope.endFlag = false;
+    $scope.startPid = '';
+    $scope.endPid = '';
     $scope.originLayer = {
         "id": "route",
         "type": "line",
@@ -41,49 +37,102 @@ tollGate.controller("tollGateController", ['$scope', 'dsEdit', '$location', func
             "line-width": 8
         }
     };
-    dsEdit.getProduct('tollgate/tollnames/31/1',  $scope.param).then(function (data) {
-        console.log(data);
-    });
-    dsEdit.getProduct('tollgate/path/2038/436328').then(function (data) {
-        var source = {
-            "type": "geojson",
-            "data": {
-                "type": "Feature",
-                "properties": {},
-                "geometry": data[0].geoJson
-            }
+    $scope.linksArr = [];
+    $scope.colorArr = ['red', 'blue', 'gray']
+    // 清空
+    $scope.clearLines = function () {
+        var geojson = {
+            "type": "FeatureCollection",
+            "features": []
         };
-        map.flyTo({center: data[0].pointGeoJson.coordinates});
-        var popup = new mapboxgl.Popup({closeOnClick: false})
-          .setLngLat(data[0].pointGeoJson.coordinates)
-          .setHTML('<h1>Hello World!</h1>')
-          .addTo(map);
-        $scope.popuArr.push(popup);
-        $scope.originLayer.source = source;
-        map.addLayer($scope.originLayer);
-    });
-    $scope.addLines = function () {
+        for (var i = 0, len = $scope.popuArr.length; i < len; i++) {
+            map.getSource('route'+i).setData(geojson);
+            $scope.popuArr[i].remove();
+        }
+        $scope.popuArr.length = 0;
+    };
+    $scope.addLines = function (data, id) {
         $scope.geojson = {
             "type": "FeatureCollection",
             "features": []
         };
-        dsEdit.getProduct('tollgate/path/2038/436328').then(function (data) {
-            var source = {
-                "type": "Feature",
-                "geometry":data[0].geoJson,
-                propertires: {
-                    id: 'test'
-                }
-            };
-            map.flyTo({center: data[0].pointGeoJson.coordinates});
-            var popup = new mapboxgl.Popup({closeOnClick: false})
-              .setLngLat(data[0].pointGeoJson.coordinates)
-              .setHTML('<h1>Hello World!</h1>')
-              .addTo(map);
-            $scope.popuArr.push(popup);
-            // $scope.originLayer.source = source;
-            $scope.geojson.features.push(source)
-            map.getSource('route').setData($scope.geojson);
+        var source = {
+            "type": "Feature",
+            "geometry": data.geoJson,
+            propertires: {
+                id: 'test'
+            }
+        };
+        $scope.createPop(data);
+        $scope.geojson.features.push(source);
+        map.getSource(id).setData($scope.geojson);
+    };
+    // 生成弹出框
+    $scope.createPop = function (data) {
+        var popup = new mapboxgl.Popup({closeOnClick: false})
+          .setLngLat(data.pointGeoJson.coordinates)
+          .setHTML('<h1>Hello World!</h1>')
+          .addTo(map);
+        $scope.popuArr.push(popup);
+    };
+    // 搜索起点
+    $scope.searchStartTollGate = function () {
+        dsEdit.getProduct('tollgate/tollnames/31/1', {name: $scope.startTollGate}).then(function (data) {
+            $scope.startFlag = true;
+            $scope.endFlag = false;
+            $scope.tollGateArr = data;
         });
-    }
+    };
+    // 搜索终点
+    $scope.searchEndTollGate = function () {
+        dsEdit.getProduct('tollgate/tollnames/sec/' + $scope.startPid + '/1', { name: $scope.endTollGate }).then(function (data) {
+            $scope.startFlag = false;
+            $scope.endFlag = true;
+            $scope.tollGateArr = data;
+        });
+    };
+    // 获取起点和重点的pid
+    $scope.getPidFromSearch = function (data) {
+     if ($scope.startFlag) {
+         $scope.startPid = data.pid;
+         $scope.startTollGate = data.name;
+         $scope.startFlag = false;
+         $scope.tollGateArr.length = 0;
+
+     }
+     if ($scope.endFlag) {
+         $scope.endPid = data.pid;
+         $scope.endTollGate = data.name;
+         $scope.endFlag = false;
+         $scope.tollGateArr.length = 0;
+     }
+    };
+    // 获取路径
+    $scope.getLinksFromStartToEnd = function () {
+        $scope.clearLines();
+        dsEdit.getProduct('tollgate/path/'+$scope.startPid+'/'+$scope.endPid).then(function (data) {
+            map.flyTo({center: data[0].pointGeoJson.coordinates});
+            $scope.linksArr = data;
+            for (var i = 0, len = data.length; i < len ;i++) {
+                if(map.getSource('route'+i)) {
+                    $scope.addLines(data[i], 'route'+i);
+                } else {
+                    var obj = $scope.originLayer;
+                    obj.id = 'route' + i;
+                    obj.paint['line-color'] = $scope.colorArr[i];
+                    var source = {
+                        "type": "geojson",
+                        "data": {
+                            "type": "Feature",
+                            "properties": {},
+                            "geometry": data[i].geoJson
+                        }
+                    };
+                    $scope.createPop(data[i]);
+                    $scope.originLayer.source = source;
+                    map.addLayer($scope.originLayer);
+                }
+            }
+        });
+    };
 }]);
