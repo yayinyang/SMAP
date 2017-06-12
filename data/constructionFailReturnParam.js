b83f862182834102aac545a160794bd70817acbc
dongjihui <dongjihui@navinfo.com>
2017-06-12 22:53:30 +0800

    api页面更新

--- data/constructionFailReturnParam.js
+++ data/constructionFailReturnParam.js
@@ -1,16 +1,73 @@
 [
   {
-  "paramName": "data",
-  "type": "string",
+  "paramName1": "data",
+  "paramName2": "----",
+  "type": "array",
  "description": "当访问出现问题时，会返回String类型的错误信息。其中包含中文（在项目中已经中文转为16进制字符，请接收端再进行转码）。"
   },
+    {
+        "paramName1":"",
+        "paramName2":"no",
+        "type": "int",
+        "description":"序号"
+    },
+    {
+        "paramName1":"",
+        "paramName2":"link_pid",
+        "type": "int",
+        "description":"LINK_PID"
+    },
+    {
+        "paramName1":"",
+        "paramName2":"geometry_link",
+        "type": "string",
+        "description":"对应的坐标，wkt格式"
+    },
+    {
+        "paramName1":"",
+        "paramName2":"construction_dir",
+        "type": "string",
+        "description":"施工方向"
+    },
+    {
+        "paramName1":"",
+        "paramName2":"admin_id",
+        "type": "int",
+        "description":"行政区划编码"
+    },
+    {
+        "paramName1":"",
+        "paramName2":"desc",
+        "type": "string",
+        "description":"施工描述"
+    },
+    {
+        "paramName1":"",
+        "paramName2":"time_domain",
+        "type": "string",
+        "description":"施工时间，时间域格式"
+    },
+    {
+        "paramName1":"",
+        "paramName2":"version",
+        "type": "string",
+        "description":"基线版本号"
+    },
+    {
+        "paramName1":"",
+        "paramName2":"sources",
+        "type": "int",
+        "description":"数据来源 （1：静态数据 2：动态数据）"
+    },
   {
-  "paramName": "errcode",
-  "type": "number",
+  "paramName1": "code",
+  "paramName2": "----",
+  "type": "int",
  "description": "返回错误码"
   },
   {
-  "paramName": "errmsg",
+  "paramName1": "msg",
+  "paramName2": "----",
   "type": "string",
  "description": "返回错误消息"
   }
