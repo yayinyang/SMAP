f2168d9ea3da2a5862c976cbdff2994f2601dc8e
dongjihui <dongjihui@navinfo.com>
2017-06-13 11:45:11 +0800

    api 内容更新

--- data/limitFailReturnParam.js
+++ data/limitFailReturnParam.js
@@ -1,17 +1,188 @@
 [
-  {
-  "paramName": "data",
-  "type": "string",
- "description": "当访问出现问题时，会返回String类型的错误信息。其中包含中文（在项目中已经中文转为16进制字符，请接收端再进行转码）。"
-  },
-  {
-  "paramName": "errcode",
-  "type": "number",
- "description": "返回错误码"
-  },
-  {
-  "paramName": "errmsg",
-  "type": "string",
- "description": "返回错误消息"
-  }
+    {
+        "paramName1": "data",
+        "paramName2": "",
+        "type": "array",
+        "description": "当访问出现问题时，会返回String类型的错误信息。其中包含中文（在项目中已经中文转为16进制字符，请接收端再进行转码）。"
+    },
+    {
+        "paramName1": "",
+        "paramName2": "no",
+        "type": "int",
+        "description": "序号"
+    },
+    {
+        "paramName1": "",
+        "paramName2": "group_id",
+        "type": "string",
+        "description": "限行策略id"
+    },
+    {
+        "paramName1": "",
+        "paramName2": "geometry",
+        "type": "string",
+        "description": "多边形的几何坐标"
+    },
+    {
+        "paramName1": "",
+        "paramName2": "link_pid",
+        "type": "int",
+        "description": "LINK_PID"
+    },
+    {
+        "paramName1": "",
+        "paramName2": "geometry_link",
+        "type": "string",
+        "description": "对应的坐标，wkt格式"
+    },
+    {
+        "paramName1": "",
+        "paramName2": "link_dir",
+        "type": "string",
+        "description": "限制方向"
+    },
+    {
+        "paramName1": "",
+        "paramName2": "admin_id",
+        "type": "int",
+        "description": "行政区划编码"
+    },
+    {
+        "paramName1": "",
+        "paramName2": "restrict",
+        "type": "string",
+        "description": "限制情况"
+    },
+    {
+        "paramName1": "",
+        "paramName2": "temp_plate",
+        "type": "int",
+        "description": "临牌转换原则"
+    },
+    {
+        "paramName1": "",
+        "paramName2": "temp_plate_num",
+        "type": "string",
+        "description": "临牌对应数字"
+    },
+    {
+        "paramName1": "",
+        "paramName2": "char_switch",
+        "type": "int",
+        "description": "字母转换原则"
+    },
+    {
+        "paramName1": "",
+        "paramName2": "char_to_num",
+        "type": "string",
+        "description": "字母对应数字"
+    },
+    {
+        "paramName1": "",
+        "paramName2": "vehicle",
+        "type": "int",
+        "description": "车辆类型"
+    },
+    {
+        "paramName1": "",
+        "paramName2": "energy_type",
+        "type": "string",
+        "description": "能源类型"
+    },
+    {
+        "paramName1": "",
+        "paramName2": "seatnum",
+        "type": "int",
+        "description": "车座限制"
+    },
+    {
+        "paramName1": "",
+        "paramName2": "platecolor",
+        "type": "int",
+        "description": "车牌颜色"
+    },
+    {
+        "paramName1": "",
+        "paramName2": "principle",
+        "type": "string",
+        "description": "限行策略的文字描述"
+    },
+    {
+        "paramName1": "",
+        "paramName2": "tail_number",
+        "type": "string",
+        "description": "尾号"
+    },
+    {
+        "paramName1": "",
+        "paramName2": "out_vehicle",
+        "type": "string",
+        "description": "是否外地车"
+    },
+    {
+        "paramName1": "",
+        "paramName2": "start_date",
+        "type": "string",
+        "description": "开始时间"
+    },
+    {
+        "paramName1": "",
+        "paramName2": "end_date",
+        "type": "string",
+        "description": "结束时间"
+    },
+    {
+        "paramName1": "",
+        "paramName2": "res_datetype",
+        "type": "string",
+        "description": "限行时间类型"
+    },
+    {
+        "paramName1": "",
+        "paramName2": "time",
+        "type": "string",
+        "description": "时间"
+    },
+    {
+        "paramName1": "",
+        "paramName2": "spec_flag",
+        "type": "string",
+        "description": "排除日期（不限行的日期类型）"
+    },
+    {
+        "paramName1": "",
+        "paramName2": "holiday",
+        "type": "string",
+        "description": "日期"
+    },
+    {
+        "paramName1": "",
+        "paramName2": "version",
+        "type": "string",
+        "description": "基线版本号"
+    },
+    {
+        "paramName1": "",
+        "paramName2": "sources",
+        "type": "int",
+        "description": "数据来源 （1：静态数据 2：动态数据）"
+    },
+    {
+        "paramName1": "",
+        "paramName2": "link_change",
+        "type": "int",
+        "description": "LINK限行变更类型（1：新增 2：变更 3：删除，即代表解除）"
+    },
+    {
+      "paramName1": "errcode",
+      "paramName2": "----",
+      "type": "number",
+      "description": "返回错误码"
+    },
+    {
+      "paramName1": "errmsg",
+      "paramName2": "----",
+      "type": "string",
+      "description": "返回错误消息"
+    }
 ]
