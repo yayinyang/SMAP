c43f43943507b7695ecd1abee42050b7d18e627e
csolin <chensl3316@outlook.com>
2017-06-28 09:51:45 +0800

    修复bug

--- js/index.js
+++ js/index.js
@@ -32,11 +32,11 @@ function setNavbarBackground() {
     var navbar = $('nav-bar');
     var body = $('body');
     var banner = $('#banner');
-    if(body[0].scrollTop > (banner[0].clientHeight -44)){ // 44px为导航栏高度
-        navbar[0].children[0].style.background = '#25396e';
-    }else {
-        navbar[0].children[0].style.background = 'none';
+    if(body && navbar[0].children[0]){
+        if(body[0].scrollTop > (banner[0].clientHeight -44)){ // 44px为导航栏高度
+            navbar[0].children[0].style.background = '#25396e';
+        }else {
+            navbar[0].children[0].style.background = 'none';
+        }
     }
-
-
 }
